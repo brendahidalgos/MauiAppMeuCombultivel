59b2f280583d4d4bf336e056fb93111fb03a4e03
brendahidalgos <brendascarante99@gmail.com>
2025-10-17 16:16:53 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMeuCombultivel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppMeuCombultivel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiAppMeuCombultivel/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppMeuCombultivel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
